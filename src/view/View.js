a09f7aed5a31f804815952ab44998966528d320e
dlsxjzld <sinjuk1@naver.com>
2024-11-29 15:36:26 +0900

    refactor(View.js): 검증 받는 부분 추상화

--- src/view/View.js
+++ src/view/View.js
@@ -1,9 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
+import { check } from '../validation/validateFunctions.js';
 
 export const Input = {
   async readUserInput(message, validation, ...rest) {
     const input = await Console.readLineAsync(message);
-    if (validation(input, rest)) {
+    if (check(input, validation, rest)) {
       return input;
     }
     return this.readUserInput(message, validation, rest);
