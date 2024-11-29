a19cbaf1fcba88b7eeb68d953e1b799758aa63a6
dlsxjzld <sinjuk1@naver.com>
2024-11-29 15:36:50 +0900

    refactor(App.js): 검증 넘기는 부분 추상화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from '@woowacourse/mission-utils';
+import { Input } from './view/View.js';
+import { purchaseMoney } from './validation/validateFunctions.js';
+
 class App {
-  async run() {}
+  async run() {
+    const input = await Input.readUserInput(
+      '구입금액을 입력해 주세요.\n',
+      purchaseMoney,
+    );
+    Console.print(input);
+  }
 }
 
 export default App;
