0cce8d5820ae1a0026eb0b8192bbc95771934a4d
dlsxjzld <sinjuk1@naver.com>
2024-12-04 20:48:46 +0900

    refactor: LottoList에서 통계 로직 제거

--- src/model/LottoList.js
+++ src/model/LottoList.js
@@ -1,24 +1,12 @@
 import { Random } from '@woowacourse/mission-utils';
 import Lotto from '../Lotto.js';
 
-import {
-  LOTTO_MATCH_KEY,
-  LOTTO_PRICE,
-  MONEY_FOR_LOTTO_WINNING_MATCH,
-} from '../constants/constant.js';
+import { LOTTO_PRICE } from '../constants/constant.js';
 
 export default class LottoList {
   #lottoCount;
 
   #lottoList;
-
-  #lottoWinningCount = {
-    3: 0,
-    4: 0,
-    5: 0,
-    bonus: 0,
-    6: 0,
-  };
 
   constructor(input) {
     this.#lottoCount = Number(input) / LOTTO_PRICE;
@@ -33,31 +21,6 @@ export default class LottoList {
   }
 
   getLottoList() {
-    return this.#lottoList.map((lotto) => lotto.getNumbers());
-  }
-
-  getLottoWinningCount() {
-    return this.#lottoWinningCount;
-  }
-
-  updateLottoWinningCount(winningNumbers, bonusNumber) {
-    this.#lottoList.forEach((lotto) => {
-      const matchCount = lotto.checkMatch(winningNumbers, bonusNumber);
-      if (this.#lottoWinningCount[matchCount] === undefined) {
-        return;
-      }
-      this.#lottoWinningCount[matchCount] += 1;
-    });
-  }
-
-  calculateProfit() {
-    let profit = 0;
-    LOTTO_MATCH_KEY.forEach((key) => {
-      profit +=
-        MONEY_FOR_LOTTO_WINNING_MATCH[key] * this.#lottoWinningCount[key];
-    });
-    return Number(
-      ((profit / (this.#lottoCount * LOTTO_PRICE)) * 100).toFixed(1),
-    );
+    return this.#lottoList;
   }
 }
